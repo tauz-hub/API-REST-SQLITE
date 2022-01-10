d9852f244d4f12ade2eac4a383a397e2e98363ff
Tauã Ferreira <86664818+tauzxd@users.noreply.github.com>
2022-01-10 16:06:56 -0300

    Delete src/secretDatabase directory

--- src/secretDatabase/configSecretDB.js
+++
@@ -1,9 +0,0 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite"
-
-export async function openDbScret() {
-  return open({
-    filename: "./src/SecretDatabase/secretDatabase.db",
-    driver: sqlite3.Database
-  })
-}
